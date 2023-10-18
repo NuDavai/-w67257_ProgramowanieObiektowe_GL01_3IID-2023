3a18e185136fbf34f17d8363d83364e9047210b6
NuDavai <116257805+NuDavai@users.noreply.github.com>
2023-10-18 14:06:03 +0200

    Add files via upload

---
+++ lab2/Licz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Models
+{
+    public class Licz
+    {
+        public int Value = 0;
+
+        public void Dodaj(int addValue)
+        {
+            Value += addValue;
+        }
+
+        public void Odejmij(int addValue)
+        {
+            Value -= addValue;
+        }
+    }
+}
---
+++ lab2/LiczTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class LiczTest
+    {
+
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+}
